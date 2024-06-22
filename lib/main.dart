33ede354360d47d882bf7bbd77cb16b49fd49ac0
brahim rida <rida.fighter@gmail.com>
2024-06-23 00:25:07 +0100

    on going overlay data

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print, no_leading_underscores_for_local_identifiers
 
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,7 +15,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        scaffoldBackgroundColor: Color.fromARGB(255, 34, 39, 46),
         useMaterial3: true,
       ),
       home: const MyHomePage(),
@@ -37,31 +38,8 @@ class _MyHomePageState extends State<MyHomePage> {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Center(
-            child: GitHubStats(
-              dataList: [
-                [
-                  {'number': 4},
-                  {'number': 0},
-                  {'number': 2},
-                  {'number': 5},
-                  {'number': 0},
-                  {'number': 10},
-                  {'number': 6},
-                  {'number': 0},
-                ],
-                [
-                  {'number': 8},
-                  {'number': 0},
-                  {'number': 2},
-                  {'number': 10},
-                  {'number': 0},
-                  {'number': 5},
-                  {'number': 6},
-                  {'number': 9},
-                ]
-              ],
-            ),
-          )
+            child: GitHubStats(data: _functionForRandomList()),
+          ),
         ],
       ),
     );
@@ -69,8 +47,8 @@ class _MyHomePageState extends State<MyHomePage> {
 }
 
 class GitHubStats extends StatefulWidget {
-  const GitHubStats({super.key, required this.dataList});
-  final List<List<Map>> dataList;
+  const GitHubStats({super.key, required this.data});
+  final List<List<Map>> data;
 
   @override
   State<GitHubStats> createState() => _GitHubStatsState();
@@ -78,108 +56,123 @@ class GitHubStats extends StatefulWidget {
 
 class _GitHubStatsState extends State<GitHubStats> {
   Color? backColor = Color.fromARGB(255, 34, 39, 46);
-  bool white = false;
-  // final LayerLink _layerLink = LayerLink();
-  // OverlayEntry? _overlayEntry;
-
-  // void _showOverlay(BuildContext context) {
-  //   OverlayState overlayState = Overlay.of(context);
-  //   _overlayEntry = OverlayEntry(
-  //     builder: (context) => Positioned(
-  //       width: 100,
-  //       child: CompositedTransformFollower(
-  //         link: _layerLink,
-  //         showWhenUnlinked: false,
-  //         offset: Offset(0.0, 50.0),
-  //         child: Material(
-  //           color: Colors.transparent,
-  //           child: BubbleWidget(),
-  //         ),
-  //       ),
-  //     ),
-  //   );
-
-  //   overlayState.insert(_overlayEntry!);
-  // }
-
-  // void _hideOverlay() {
-  //   _overlayEntry?.remove();
-  //   // _overlayEntry = null;
-  // }
-
-  void changeBackgroundColor() {
-    setState(() {
-      if (!white) {
-        backColor = Colors.white;
-        white = true;
-      } else {
-        backColor = Colors.grey[850];
-        white = false;
-      }
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height * 0.2;
-    double width = MediaQuery.of(context).size.width * 0.7;
-    return Container(
-      decoration: BoxDecoration(
-        color: backColor,
-        borderRadius: BorderRadius.circular(
-          12,
-        ),
-      ),
-      height: height,
-      width: width,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ...List.generate(
-            widget.dataList.length,
-            (index) => pillar(
-              widget.dataList[index],
-              context,
-              changeBackgroundColor,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Mon',
+                  style: TextStyle(
+                      color: const Color.fromARGB(255, 197, 209, 222),
+                      fontSize: 12,
+                      fontFamily: 'apple-system'),
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  'Wed',
+                  style: TextStyle(
+                      color: const Color.fromARGB(255, 197, 209, 222),
+                      fontSize: 12,
+                      fontFamily: 'apple-system'),
+                ),
+                SizedBox(
+                  height: 6,
+                ),
+                Text(
+                  'Fri',
+                  style: TextStyle(
+                      color: const Color.fromARGB(255, 197, 209, 222),
+                      fontSize: 12,
+                      fontFamily: 'apple-system'),
+                ),
+              ],
             ),
-          )
-        ],
-      ),
+            SizedBox(
+              width: 4,
+            ),
+            ...List.generate(
+              widget.data.length,
+              (index) => pillar(
+                widget.data[index],
+                context,
+              ),
+            )
+          ],
+        ),
+      ],
     );
   }
 }
 
-Widget pillar(List<Map> list, BuildContext context, Function changebgColor) {
+Widget pillar(
+  List<Map> list,
+  BuildContext context,
+) {
   return Column(
+    mainAxisSize: MainAxisSize.min,
     children: [
       ...List.generate(
         list.length,
-        (index) => smallSquare(list[index]['number'], context, changebgColor),
+        (index) => smallSquare(list[index]['number'], context),
       ),
     ],
   );
 }
 
-Widget smallSquare(int number, BuildContext context, Function changebgColor) {
-  double calculation = (1 - number / 10) * 255;
+Widget smallSquare(int number, BuildContext context) {
+  double width = MediaQuery.of(context).size.width;
+  int dataMaxValue = 10;
+  double calculation = (number / dataMaxValue) * 255;
   int degree = calculation.round();
-  print("the degree: $degree");
   Color color = degree > 0
       ? Color.fromARGB(degree, 0, 255, 0)
       : Color.fromARGB(255, 22, 27, 34);
 
   return GestureDetector(
     onTap: () {
-      changebgColor();
-      print('click');
+      OverlayEntry overlayEntry = OverlayEntry(
+        builder: (context) => Center(
+          child: Material(
+            color: Colors.transparent,
+            child: Container(
+              width: 200,
+              height: 30,
+              color: Colors.blue,
+              child: Center(
+                child: Text(
+                  'this is width: $width Number of contributions: $number',
+                  style: TextStyle(color: Colors.white, fontSize: 11),
+                ),
+              ),
+            ),
+          ),
+        ),
+      );
+
+      Overlay.of(context).insert(overlayEntry);
+
+      // Close overlay after 2 seconds
+      Future.delayed(const Duration(seconds: 2), () {
+        overlayEntry.remove();
+      });
     },
     child: Padding(
       padding: const EdgeInsets.all(1),
       child: Container(
         decoration:
             BoxDecoration(color: color, borderRadius: BorderRadius.circular(2)),
-        height: 10,
-        width: 10,
+        height: width > 710 ? 11 : 7,
+        width: width > 710 ? 11 : 7,
       ),
     ),
   );
@@ -190,6 +183,22 @@ class BubbleWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+        height: 50, width: 50, color: Colors.red, child: Text("hey"));
   }
+}
+
+List<List<Map>> _functionForRandomList() {
+  const int weeks = 52;
+  const int days = 7;
+
+  List<List<Map>> list = [];
+  for (int i = 0; i < weeks; i++) {
+    List<Map<String, int>> nested = [];
+    for (int j = 0; j < days; j++) {
+      nested.add({'number': Random().nextInt(11)});
+    }
+    list.add(nested);
+  }
+  return list;
 }
